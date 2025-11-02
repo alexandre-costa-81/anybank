c763af784c2fb8e5cc3f986d4f1888e23b394d41
Alexandre Costa <alexandre.costa@furg.br>
2025-11-02 00:12:02 -0300

    feat: bind transaction value input and implement form submission logic

--- src/app/form-nova-transacao/form-nova-transacao.ts
+++ src/app/form-nova-transacao/form-nova-transacao.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.html',
   styleUrl: './form-nova-transacao.css',
 })
 export class FormNovaTransacao {
+  valorTransacao: number | null = 40;
 
+  aoSubmeter() {
+    console.log('Transação submetida com valor:', this.valorTransacao);
+  }
 }
