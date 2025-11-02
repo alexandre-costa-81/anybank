41aca94c451d09ad3699021cd86cd4a2732e957e
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 23:40:18 -0300

    feat: enhance boas-vindas and conta components with title casing and currency formatting

--- src/app/banner/conta/conta.ts
+++ src/app/banner/conta/conta.ts
@@ -1,8 +1,9 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-conta',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './conta.html',
   styleUrl: './conta.css',
 })
