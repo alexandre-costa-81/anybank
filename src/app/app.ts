18e9498359dc90862ee0bbc134623db198b36fbd
Alexandre Costa <alexandre.costa@furg.br>
2025-11-02 11:18:42 -0300

    feat: integrate CurrencyPipe and update saldo computation in App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Banner } from "./banner/banner";
 import { FormNovaTransacao } from "./form-nova-transacao/form-nova-transacao";
 import { Transacao } from './modelos/transacao';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Banner, FormNovaTransacao],
+  imports: [Banner, FormNovaTransacao, CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   transacoes = signal<Transacao[]>([]);
+  saldo = computed(() => {
+    return this.transacoes().reduce((total, transacao) => {
+      if (transacao.tipo === "deposito") {
+        return total + transacao.valor;
+      } else {
+        return total - transacao.valor;
+      }
+    }, 0);
+  });
 
   processarTransacao(transacao: Transacao) {
     this.transacoes.update(transacoesAtuais => [transacao, ...transacoesAtuais]);
-
-    console.log(this.transacoes());
   }
 }
