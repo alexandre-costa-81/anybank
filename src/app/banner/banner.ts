3de0df036364908174e38f796e59891995ee4d65
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 23:19:41 -0300

    feat: bind current date to banner component

--- src/app/banner/banner.ts
+++ src/app/banner/banner.ts
@@ -1,11 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './banner.html',
   styleUrl: './banner.css',
 })
 export class Banner {
-
+  dataAtual = new Date();
 }
